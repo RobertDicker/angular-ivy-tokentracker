50c0755630038c73abba2eef50bea0e9e867116e
RobertDicker <robbiedicker@gmail.com>
2021-02-09 13:25:39 +1030

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { TokenMainComponent } from './token-main/token-main.component';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,  RouterModule.forRoot([
+      { path: '', component: TokenMainComponent },
+      
+    ]) ],
+  
+  
+  declarations: [ AppComponent, TopBarComponent, TokenMainComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/token-main/token-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-main',
+  templateUrl: './token-main.component.html',
+  styleUrls: ['./token-main.component.css']
+})
+export class TokenMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
